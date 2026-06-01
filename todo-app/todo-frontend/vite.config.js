ddd11099b6720a58c6856529ff534fd747fa0928
rafaeltorok <rafaeltorok@gmail.com>
2026-06-01 17:41:42 -0300

    refactor: use relative API paths for frontend requests

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const serverUrl = process.env.VITE_BACKEND_URL || "http://localhost:3000"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/todos": {
+        target: serverUrl,
+        changeOrigin: true,
+      }
+    }
+  }
 })
